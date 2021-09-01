6848caa33e591a4a2e5253f7eff2ebd8be40a92e
Nguyen Anh Tuan <33346539+greny98@users.noreply.github.com>
2021-09-01 17:22:19 +0700

    fix text to speech

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 1. Library
-- `gtts`: Convert Text to Speech
+- `pyttsx3`: Convert Text to Speech
 - `SpeechRecognition`: Convert Speech to Text
 - `fuzzywuzzy`: Calculate the similarity of two text
 - `playsound`: Open audio file in app
@@ -9,10 +9,9 @@
 - Prepare questions and corresponding answers for each question.
 - **Questions** in `Question.txt` and **Answers** in `Answer.txt`
 - Each question and each answer is only on one line
-- We will use the library `gtts` to create a **voice file** for each answer. Moreover, We will also create voice files for greetings and goodbyes
 ## 2.2. Start up
 - Read all questions from the file `Question.txt`. Since each question is only on one line, we will separate the content by a newline character `\n`.
-- Open greetings voice file
+- Use `pyttsx3` to greetings
 - Start microphone and filter noise
 ## 2.3. Communicating with visitors
 - Visitor asks and the system will record the sound through the **Microphone**
@@ -20,5 +19,5 @@
 - For each question in the list, we use the library `fuzzywuzzy` to calculate the similarity to the visitor's question.
 - Find the question with the highest similarity.
     - If this similarity is less than 70% then we will conclude that none of the questions in the list match and open audio file to say "I don't know"
-    - Otherwise we will open the audio file of the corresponding answer
+    - Otherwise, we will open the audio file of the corresponding answer
 - Ask if the visitor wants to know any other information, otherwise will open the audio file goodbye, otherwise will return to the first step
--- assistant_test.py
+++ assistant_test.py
@@ -2,40 +2,49 @@ import speech_recognition as sr
 from assistant import utils
 import time
 from fuzzywuzzy import fuzz
+import pyttsx3
+
+
+def say(text, engine):
+    engine.say(text)
+    engine.runAndWait()
+
 
 if __name__ == '__main__':
     questions = utils.load_questions()
     answers = utils.load_answers()
     recognizer = sr.Recognizer()
     microphone = sr.Microphone()
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 150)
+
     with microphone as source:
         recognizer.adjust_for_ambient_noise(source)  # loc nhieu
         is_communicate = True
         count_failed = 0
-        utils.hello()
+        say("hello, what information would you like to know?", engine)
         while is_communicate:
-            utils.knowledge()
             print("Listening...")
             audio = recognizer.listen(source, timeout=6, phrase_time_limit=5)
             try:
                 print('Waiting...')
-                utils.wait()
+                say("please wait a moment", engine)
                 ques = recognizer.recognize_google(audio, language='vi')
                 print("Question: ", ques)
                 ques_num = utils.find_question_num(ques, questions)
                 print("Question num: ", ques_num)
                 if ques_num != -1:
                     print("Answer: ", answers[ques_num])
-                utils.answer(ques_num)
-                utils.change_question()
+                say(answers[ques_num], engine)
+                say('do you want any other information?', engine)
                 print("Listening...")
                 audio = recognizer.listen(source)
                 next = recognizer.recognize_google(audio, language='vi')
                 next = utils.remove_accents(next.lower())
-                if (fuzz.ratio(next, 'khong') >= 70 or
-                    fuzz.ratio(next, 'thoi') >= 70 or
+                if (fuzz.ratio(next, 'no thank') >= 70 or
+                    fuzz.ratio(next, 'no') >= 70 or
                     fuzz.ratio(next, 'toi khong nhe') >= 70):
-                    utils.good_bye()
+                    say('Goodbye, have a nice day', engine)
                     break
             except sr.RequestError:  # Service/network error
                 print("API unavailable")
